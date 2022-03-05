941f13d43703248ea47355063825015d661cff14
ojas604 <98620101+ojas604@users.noreply.github.com>
2022-03-05 18:01:37 -0500

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Models.swift
+//  Bullseye
+//
+//  Created by Ojas Jain on 8/11/21.
+//
+
+import Foundation
+import UIKit
+
+enum Constants {
+    enum General {
+        public static let strokeWidth = CGFloat(2.0)
+        public static let roundedViewLength = CGFloat(56.0)
+        public static let roundRectViewWidth = CGFloat(68.0)
+        public static let roundRectViewHeight = CGFloat(56.0)
+        public static let roundRectCornerRadius = CGFloat(21.0)
+
+    }
+}
