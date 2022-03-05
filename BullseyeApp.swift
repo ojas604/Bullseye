d10118a24dcef737707481ad436b92232ddba1ea
ojas604 <98620101+ojas604@users.noreply.github.com>
2022-03-05 17:56:44 -0500

    Add files via upload

---
+++ BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Ojas Jain on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
